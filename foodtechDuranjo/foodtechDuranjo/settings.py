8b012f70b9a8de0cc7377f328030e053623c0d62
José Ordoñez <restonjd@gmail.com>
2013-10-28 15:51:29 -0430

    listo para empezar a maquetar

--- foodtechDuranjo/foodtechDuranjo/settings.py
+++ foodtechDuranjo/foodtechDuranjo/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -112,6 +112,7 @@ ROOT_URLCONF = 'foodtechDuranjo.urls'
 WSGI_APPLICATION = 'foodtechDuranjo.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -130,6 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'foodtechDuranjo.apps.homepage',
 )
 
 # A sample logging configuration. The only tangible logging
--- foodtechDuranjo/foodtechDuranjo/urls.py
+++ foodtechDuranjo/foodtechDuranjo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from sitemaps import StaticViewSitemap
+from django.http import HttpResponse
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -13,10 +14,13 @@ urlpatterns = patterns(
 	# Examples:
 	# url(r'^$', 'foodtechDuranjo.views.home', name='home'),
 	# url(r'^foodtechDuranjo/', include('foodtechDuranjo.foo.urls')),
-
+	url(r'^', include('foodtechDuranjo.apps.homepage.urls')),
 	# Uncomment the admin/doc line below to enable admin documentation:
 	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+	(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+	(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap', {'sitemaps': sitemaps}),
+	#Google Web master Tool
+	(r'^googlec34fe789f50fc843\.html$', lambda r: HttpResponse("google-site-verification: googlec34fe789f50fc843.html", mimetype="text/plain")),
 	# Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
@@ -26,7 +30,11 @@ urlpatterns = patterns(
 )
 
 if settings.DEBUG:
+	from django.views.generic import TemplateView
 	urlpatterns += patterns(
 		'',
+		url(r'^404/$', TemplateView.as_view(template_name="404.html")),
+		url(r'^403/$', TemplateView.as_view(template_name="403.html")),
+		(r'^500/$', TemplateView.as_view(template_name="500.html")),
 		(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 	)
